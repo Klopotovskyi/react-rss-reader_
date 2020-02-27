f50f5f636ad912a05c6d98dbb773ab9c0c1d670e
Klopotovskyi <patola@ukr.net>
2020-02-27 12:35:11 +0200

    fix streamlist test

--- src/modules/StreamList/__tests__/StreamList.test.tsx
+++ src/modules/StreamList/__tests__/StreamList.test.tsx
@@ -6,24 +6,36 @@ import configureMockStore from 'redux-mock-store'
 import {apiMiddleware, RSAA} from 'redux-api-middleware'
 import thunk from 'redux-thunk'
 import fetchMock from 'fetch-mock'
-import '../services/actions'
-import {LOAD_STREAMS_FAILURE, LOAD_STREAMS_REQUEST, LOAD_STREAMS_SUCCESS} from '../services/constants';
+
+import {
+    ADD_STREAM_SUCCESS,
+    LOAD_STREAMS_FAILURE,
+    LOAD_STREAMS_REQUEST,
+    LOAD_STREAMS_SUCCESS
+} from '../services/constants';
+import {addStream} from '../services/actions';
+
 const middlewares = [thunk, apiMiddleware];
 const mockStore = configureMockStore(middlewares);
 
-
+// jest.mock('../services/actions', () => ({
+//     addStream: jest.fn()
+// }));
 describe('should dispatch LOAD_STREAMS_SUCCESS when loadStreams is called', () => {
-
+    const body = ["https://fakty.ua/rss_feed/science",
+        "https://www.radiosvoboda.org/api/zrqiteuuir/",
+        "http://4pda.ru/feed/",
+        "https://www.pravda.com.ua/rss/view_news/",]
+    ;
+    const store = mockStore({streamList: body});
+    const getMockResponse = () => {
+        return fetchMock.getOnce(`https://rss-reader-676d2.firebaseio.com/TestArray.json`, {headers: {'content-type': 'application/json'}});
+    };
     describe('when rendered', () => {
 
         it('should not be active saved button', () => {
             const store = mockStore({});
-            const body = ["https://fakty.ua/rss_feed/science",
-                "https://www.radiosvoboda.org/api/zrqiteuuir/",
-                "http://4pda.ru/feed/",
-                "https://www.pravda.com.ua/rss/view_news/",]
-            ;
-            fetchMock.getOnce(`https://rss-reader-676d2.firebaseio.com/TestArray.json`, {headers: {'content-type': 'application/json'}});
+            getMockResponse();
             const expectedActions = [
                 {type: LOAD_STREAMS_REQUEST},
                 {type: LOAD_STREAMS_SUCCESS, payload: body}
@@ -44,13 +56,7 @@ describe('should dispatch LOAD_STREAMS_SUCCESS when loadStreams is called', () =
         it('should add input url to list of streams', () => {
             fetchMock.reset();
             fetchMock.restore();
-            const body = ["https://fakty.ua/rss_feed/science",
-                "https://www.radiosvoboda.org/api/zrqiteuuir/",
-                "http://4pda.ru/feed/",
-                "https://www.pravda.com.ua/rss/view_news/",]
-            ;
-            const store = mockStore({streamList: body});
-            fetchMock.getOnce(`https://rss-reader-676d2.firebaseio.com/TestArray.json`, {headers: {'content-type': 'application/json'}});
+            getMockResponse();
             return store.dispatch({
                 [RSAA]: {
                     endpoint: 'https://rss-reader-676d2.firebaseio.com/TestArray.json',
@@ -60,10 +66,20 @@ describe('should dispatch LOAD_STREAMS_SUCCESS when loadStreams is called', () =
             }).then(() => {
                 const wrapper = mount(<Provider store={store}><StreamList/></Provider>);
                 wrapper.find('.add-stream-btn').simulate('click');
+                wrapper.find('input').simulate('change', {
+                    currentTarget: {
+                        value: 'newStream'
+                    }
+                });
+                expect(addStream).toHaveBeenCalledWith({
+                    payload: 'newStream'
+                });
+
                 expect(wrapper.find('.list-of-streams').children().length).toEqual(4);
                 fetchMock.reset();
                 fetchMock.restore();
             });
+
 
         });
     });
