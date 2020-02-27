4a787c26067476bc9d6b3327cb867a367f3215b9
Klopotovskyi <patola@ukr.net>
2020-02-27 06:08:45 +0200

    fix test

--- src/modules/StreamList/__tests__/StreamList.test.tsx
+++ src/modules/StreamList/__tests__/StreamList.test.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 import StreamList from '../StreamList'
 import {mount, ReactWrapper, shallow} from 'enzyme';
 import {Provider} from 'react-redux';
-import configureStore from 'redux-mock-store'
 import configureMockStore from 'redux-mock-store'
 import {apiMiddleware, RSAA} from 'redux-api-middleware'
 import thunk from 'redux-thunk'
 import fetchMock from 'fetch-mock'
 import '../services/actions'
-import {loadStreams} from '../services/actions'
 import {LOAD_STREAMS_FAILURE, LOAD_STREAMS_REQUEST, LOAD_STREAMS_SUCCESS} from '../services/constants';
-
 const middlewares = [thunk, apiMiddleware];
 const mockStore = configureMockStore(middlewares);
 
@@ -45,7 +42,8 @@ describe('should dispatch LOAD_STREAMS_SUCCESS when loadStreams is called', () =
     describe('when add button clicked', () => {
 
         it('should add input url to list of streams', () => {
-
+            fetchMock.reset();
+            fetchMock.restore();
             const body = ["https://fakty.ua/rss_feed/science",
                 "https://www.radiosvoboda.org/api/zrqiteuuir/",
                 "http://4pda.ru/feed/",
@@ -53,10 +51,6 @@ describe('should dispatch LOAD_STREAMS_SUCCESS when loadStreams is called', () =
             ;
             const store = mockStore({streamList: body});
             fetchMock.getOnce(`https://rss-reader-676d2.firebaseio.com/TestArray.json`, {headers: {'content-type': 'application/json'}});
-            // const expectedActions = [
-            //     {type: LOAD_STREAMS_REQUEST},
-            //     {type: LOAD_STREAMS_SUCCESS, payload: body}
-            // ];
             return store.dispatch({
                 [RSAA]: {
                     endpoint: 'https://rss-reader-676d2.firebaseio.com/TestArray.json',
@@ -65,9 +59,11 @@ describe('should dispatch LOAD_STREAMS_SUCCESS when loadStreams is called', () =
                 }
             }).then(() => {
                 const wrapper = mount(<Provider store={store}><StreamList/></Provider>);
-                wrapper.find('button').simulate('click');
-                expect(wrapper.find('.list-of-streams').length).toEqual(5)
-            })
+                wrapper.find('.add-stream-btn').simulate('click');
+                expect(wrapper.find('.list-of-streams').children().length).toEqual(4);
+                fetchMock.reset();
+                fetchMock.restore();
+            });
 
         });
     });
